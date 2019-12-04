e4f94f4baeca4861b9eebc9cc29f10e4fc803537
root <root@mariguella.kali>
2019-12-04 20:16:55 +0100

    nest installed

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,30 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-AUTHOR = "Mikael Monjour"
-SITENAME = "Blog de Mikael"
-SITEURL = ""
-
-PATH = "content"
-
-TIMEZONE = "Europe/Paris"
-
-DEFAULT_LANG = "fr"
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
-
 # NEST Template
 THEME = 'nest'
 SITESUBTITLE = u'My Awesome Blog'
