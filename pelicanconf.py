c450c659f7349001efe5fa42a172a7449ec5c8f1
root <root@mariguella.kali>
2019-12-04 20:31:04 +0100

    nest installed

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,87 +3,87 @@
 from __future__ import unicode_literals
 
 # NEST Template
-THEME = 'nest'
-SITESUBTITLE = u'My Awesome Blog'
+THEME = "nest"
+SITESUBTITLE = "My Awesome Blog"
 # Minified CSS
 NEST_CSS_MINIFY = True
 # Add canonical link element to top page header and all article/author/category/tag page header
 NEST_REL_CANONICAL_LINK = True
 # Add items to top menu before pages
-MENUITEMS = [('Homepage', '/'),('Categories','/categories.html')]
-# Add header background image from content/images : 'background.jpg'
-NEST_HEADER_IMAGES = ''
-NEST_HEADER_LOGO = '/image/logo.png'
+MENUITEMS = [("Homepage", "/"),("Categories","/categories.html")]
+# Add header background image from content/images : "background.jpg"
+NEST_HEADER_IMAGES = ""
+NEST_HEADER_LOGO = "/image/logo.png"
 # Footer
-NEST_SITEMAP_COLUMN_TITLE = u'Sitemap'
-NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html'), ('Authors','/authors.html')]
-NEST_SITEMAP_ATOM_LINK = u'Atom Feed'
-NEST_SITEMAP_RSS_LINK = u'RSS Feed'
-NEST_SOCIAL_COLUMN_TITLE = u'Social'
-NEST_LINKS_COLUMN_TITLE = u'Links'
-NEST_COPYRIGHT = u'&copy; blogname 2015'
+NEST_SITEMAP_COLUMN_TITLE = "Sitemap"
+NEST_SITEMAP_MENU = [("Archives", "/archives.html"),("Tags","/tags.html"), ("Authors","/authors.html")]
+NEST_SITEMAP_ATOM_LINK = "Atom Feed"
+NEST_SITEMAP_RSS_LINK = "RSS Feed"
+NEST_SOCIAL_COLUMN_TITLE = "Social"
+NEST_LINKS_COLUMN_TITLE = "Links"
+NEST_COPYRIGHT = "&copy; blogname 2015"
 # Footer optional
-NEST_FOOTER_HTML = ''
+NEST_FOOTER_HTML = ""
 # index.html
-NEST_INDEX_HEAD_TITLE = u'Homepage'
-NEST_INDEX_HEADER_TITLE = u'My Awesome Blog'
-NEST_INDEX_HEADER_SUBTITLE = u'Smashing The Stack For Fun And Profit'
-NEST_INDEX_CONTENT_TITLE = u'Last Posts'
+NEST_INDEX_HEAD_TITLE = "Homepage"
+NEST_INDEX_HEADER_TITLE = "My Awesome Blog"
+NEST_INDEX_HEADER_SUBTITLE = "Smashing The Stack For Fun And Profit"
+NEST_INDEX_CONTENT_TITLE = "Last Posts"
 # archives.html
-NEST_ARCHIVES_HEAD_TITLE = u'Archives'
-NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
-NEST_ARCHIVES_HEADER_TITLE = u'Archives'
-NEST_ARCHIVES_HEADER_SUBTITLE = u'Archives for all posts'
-NEST_ARCHIVES_CONTENT_TITLE = u'Archives'
+NEST_ARCHIVES_HEAD_TITLE = "Archives"
+NEST_ARCHIVES_HEAD_DESCRIPTION = "Posts Archives"
+NEST_ARCHIVES_HEADER_TITLE = "Archives"
+NEST_ARCHIVES_HEADER_SUBTITLE = "Archives for all posts"
+NEST_ARCHIVES_CONTENT_TITLE = "Archives"
 # article.html
-NEST_ARTICLE_HEADER_BY = u'By'
-NEST_ARTICLE_HEADER_MODIFIED = u'modified'
-NEST_ARTICLE_HEADER_IN = u'in category'
+NEST_ARTICLE_HEADER_BY = "By"
+NEST_ARTICLE_HEADER_MODIFIED = "modified"
+NEST_ARTICLE_HEADER_IN = "in category"
 # author.html
-NEST_AUTHOR_HEAD_TITLE = u'Posts by'
-NEST_AUTHOR_HEAD_DESCRIPTION = u'Posts by'
-NEST_AUTHOR_HEADER_SUBTITLE = u'Posts archives'
-NEST_AUTHOR_CONTENT_TITLE = u'Posts'
+NEST_AUTHOR_HEAD_TITLE = "Posts by"
+NEST_AUTHOR_HEAD_DESCRIPTION = "Posts by"
+NEST_AUTHOR_HEADER_SUBTITLE = "Posts archives"
+NEST_AUTHOR_CONTENT_TITLE = "Posts"
 # authors.html
-NEST_AUTHORS_HEAD_TITLE = u'Author list'
-NEST_AUTHORS_HEAD_DESCRIPTION = u'Author list'
-NEST_AUTHORS_HEADER_TITLE = u'Author list'
-NEST_AUTHORS_HEADER_SUBTITLE = u'Archives listed by author'
+NEST_AUTHORS_HEAD_TITLE = "Author list"
+NEST_AUTHORS_HEAD_DESCRIPTION = "Author list"
+NEST_AUTHORS_HEADER_TITLE = "Author list"
+NEST_AUTHORS_HEADER_SUBTITLE = "Archives listed by author"
 # categories.html
-NEST_CATEGORIES_HEAD_TITLE = u'Categories'
-NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
-NEST_CATEGORIES_HEADER_TITLE = u'Categories'
-NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+NEST_CATEGORIES_HEAD_TITLE = "Categories"
+NEST_CATEGORIES_HEAD_DESCRIPTION = "Archives listed by category"
+NEST_CATEGORIES_HEADER_TITLE = "Categories"
+NEST_CATEGORIES_HEADER_SUBTITLE = "Archives listed by category"
 # category.html
-NEST_CATEGORY_HEAD_TITLE = u'Category Archive'
-NEST_CATEGORY_HEAD_DESCRIPTION = u'Category Archive'
-NEST_CATEGORY_HEADER_TITLE = u'Category'
-NEST_CATEGORY_HEADER_SUBTITLE = u'Category Archive'
+NEST_CATEGORY_HEAD_TITLE = "Category Archive"
+NEST_CATEGORY_HEAD_DESCRIPTION = "Category Archive"
+NEST_CATEGORY_HEADER_TITLE = "Category"
+NEST_CATEGORY_HEADER_SUBTITLE = "Category Archive"
 # pagination.html
-NEST_PAGINATION_PREVIOUS = u'Previous'
-NEST_PAGINATION_NEXT = u'Next'
+NEST_PAGINATION_PREVIOUS = "Previous"
+NEST_PAGINATION_NEXT = "Next"
 # period_archives.html
-NEST_PERIOD_ARCHIVES_HEAD_TITLE = u'Archives for'
-NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = u'Archives for'
-NEST_PERIOD_ARCHIVES_HEADER_TITLE = u'Archives'
-NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = u'Archives for'
-NEST_PERIOD_ARCHIVES_CONTENT_TITLE = u'Archives for'
+NEST_PERIOD_ARCHIVES_HEAD_TITLE = "Archives for"
+NEST_PERIOD_ARCHIVES_HEAD_DESCRIPTION = "Archives for"
+NEST_PERIOD_ARCHIVES_HEADER_TITLE = "Archives"
+NEST_PERIOD_ARCHIVES_HEADER_SUBTITLE = "Archives for"
+NEST_PERIOD_ARCHIVES_CONTENT_TITLE = "Archives for"
 # tag.html
-NEST_TAG_HEAD_TITLE = u'Tag archives'
-NEST_TAG_HEAD_DESCRIPTION = u'Tag archives'
-NEST_TAG_HEADER_TITLE = u'Tag'
-NEST_TAG_HEADER_SUBTITLE = u'Tag archives'
+NEST_TAG_HEAD_TITLE = "Tag archives"
+NEST_TAG_HEAD_DESCRIPTION = "Tag archives"
+NEST_TAG_HEADER_TITLE = "Tag"
+NEST_TAG_HEADER_SUBTITLE = "Tag archives"
 # tags.html
-NEST_TAGS_HEAD_TITLE = u'Tags'
-NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
-NEST_TAGS_HEADER_TITLE = u'Tags'
-NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
-NEST_TAGS_CONTENT_TITLE = u'Tags List'
-NEST_TAGS_CONTENT_LIST = u'tagged'
+NEST_TAGS_HEAD_TITLE = "Tags"
+NEST_TAGS_HEAD_DESCRIPTION = "Tags List"
+NEST_TAGS_HEADER_TITLE = "Tags"
+NEST_TAGS_HEADER_SUBTITLE = "Tags List"
+NEST_TAGS_CONTENT_TITLE = "Tags List"
+NEST_TAGS_CONTENT_LIST = "tagged"
 # Static files
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/logo.svg']
+STATIC_PATHS = ["images", "extra/robots.txt", "extra/favicon.ico", "extra/logo.svg"]
 EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/logo.svg': {'path': 'logo.svg'}
+    "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/logo.svg": {"path": "logo.svg"}
 }
